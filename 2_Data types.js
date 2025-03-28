1faaeebb70925f983e43ca6a2deccba0c0ba2ecd
MashkinA <cool.solt12@ya.ru>
2025-03-28 22:22:11 +0300

    edit

---
+++ 2_Data types.js
@@ -0,0 +1,31 @@
+// Типы данных
+
+// Числа
+
+const a = 1;
+console.log(a);
+
+// Строки
+// String
+
+const name = 'String';
+console.log(name);
+
+// Истина-Ложь (булевые значения)
+// true or false
+// Boolean
+
+const trueOrfalse = true;
+console.log(trueOrfalse);
+
+// Undefined
+
+let b;
+console.log(b);
+
+// Null
+
+let c = null;
+console.log(c);
+
+// Другие типы данных
