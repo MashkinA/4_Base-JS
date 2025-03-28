e2fc0620f0f81404befe3a1c2773bf0f5223a541
MashkinA <cool.solt12@ya.ru>
2025-03-28 21:35:36 +0300

    rename 1st file

---
+++ 1_Variable.js
@@ -0,0 +1,30 @@
+// Что такое переменная
+
+let a = 1;
+console.log(a);
+
+// Как используются переменные для хранения данных
+
+// Объявление переменной
+
+let numTwo;
+
+// Инициализация переменной
+
+numTwo = 2;
+console.log(numTwo);
+
+//  Объявление + инициализация
+
+let numThree = 3;
+console.log(numThree);
+
+// Изменение переменной
+
+numTwo = 2;
+console.log(numTwo);
+
+// Объявление константы
+
+const numFour = 4;
+console.log(numFour);
--- index.js
+++
@@ -1,4 +0,0 @@
-
-        const h1 = document.querySelector('h1');
-        h1.innerText = 'Title';
-        h1.style.color = 'red';
