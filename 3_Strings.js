1cc0067f7684781031b1a293db3a0e540a6c52ea
MashkinA <cool.solt12@ya.ru>
2025-03-29 15:58:35 +0300

    education

--- 3_Strings.js
+++ 3_Strings.js
@@ -1,16 +1,23 @@
 // Строки
-
-
 // Создание строки
 
+const name = 'Alexander';
+console.log(name);
 
 // '' or ""
 
+const surname = "Mashkin";
+console.log(surname);
 
 // Объединение строк (конкатенация)
 
+console.log(name+surname);
 
 // Шаблоные строки
 
+const fullname = `Name Family: ${name} ${surname}`;
+console.log(fullname);
 
 // Длина строки
+
+console.log(name.length);
