7f5029a11418d31b5082955103fcc3b27fb3fdb0
MashkinA <cool.solt12@ya.ru>
2025-03-29 16:17:13 +0300

    education

--- 4_Math op.js
+++ 4_Math op.js
@@ -1,11 +1,24 @@
 // Арифметические операторы
 
+const a = 5;
+const b = 2;
+const c = a+b;
 // Сложение
+
+console.log(a+b);
 
 // Вычитание
 
+console.log(b-a);
+
 // Умножение
+
+console.log(a*b);
 
 // Деление
 
+console.log(a/b);
+
 // Сложные выражения
+
+console.log((a+1)*4);
