7987519d1faaeef6bfadd4a19a171ab868a9c574
MashkinA <cool.solt12@ya.ru>
2025-03-30 23:06:48 +0300

    ed

--- 8_Function.js
+++ 8_Function.js
@@ -1,10 +1,45 @@
 // Функция
 
+const c = 6;
 
 // Создание функции
 
+function sayHi() {
+    console.log('Hello');    
+}
+
+sayHi();
 
 // Передача параметров
 
+sum(1, '3');
+
+function sum(a, b) {
+    const result = a + b;
+    console.log(result);
+    console.log(result + c);
+}
 
 // Возвращение значений
+
+const name = 'bob';
+const surname = 'smith';
+const fn = fullname(name, surname);
+
+console.log(fn);
+
+function fullname(name, surname) {
+    return `${name} ${surname}`;
+    
+}
+
+// 
+
+const myage = 24;
+const cbf = adult(myage);
+
+console.log(cbf);
+
+function adult(age) {
+    return age > 18;
+}
---
+++ 9_Checkpoint.js
@@ -0,0 +1,88 @@
+// 1
+
+multiple(2.5, 3);
+
+function multiple(a, b) {
+    const result = a * b;
+    console.log(result);
+}
+
+// 2
+
+const day = 3;
+const month = 6;
+const year = 2023;
+buildDate(day, month, year);
+
+function buildDate(day, month, year) {
+    const date = `${day}/${month}/${year}`;
+    console.log(date);
+}
+
+// 3
+
+const age = 17;
+isAdult(age);
+
+function isAdult(age) {
+    const result = (age >= 18);
+    console.log(result);
+}
+
+// 4
+
+const month11 = 5;
+getMonthByNumber(month11);
+
+function getMonthByNumber(monthnumber) {
+    
+    switch (monthnumber) {
+        case (monthnumber = 1):
+            console.log('Январь');
+            break;
+        case (monthnumber = 2):
+            console.log('Февраль');
+            break;
+        case (monthnumber = 3):
+            console.log('Март');
+            break;
+        case (monthnumber = 4):
+            console.log('Апрель');
+            break;
+        case (monthnumber = 5):
+            console.log('Май');
+            break;
+        case (monthnumber = 6):
+            console.log('Июнь');
+            break;
+      }
+}
+
+// 5
+
+const rub = 23322;
+const othervalue = 'EUR';
+
+convertRub(rub, othervalue);
+
+function convertRub(valuerub, currency) {
+    
+    switch (currency) {
+        case (currency = 'USD'):
+            const USD = (valuerub * 100);
+            console.log(USD);
+            break;
+        case (currency = 'EUR'):
+            const EUR = (valuerub * 90);
+            console.log(EUR);
+            break;
+        case (currency = 'KZT'):
+            const KZT = valuerub*0.17;
+            console.log(KZT);
+            break;
+        case (currency = 'AED'):
+            const AED = valuerub*23;
+            console.log(AED);
+            break;
+      }
+}
