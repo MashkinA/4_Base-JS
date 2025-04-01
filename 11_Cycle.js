c8928b6c3b384fee9fd4c70bbcf2a27da06d07f7
MashkinA <cool.solt12@ya.ru>
2025-04-01 20:37:01 +0300

    ed

--- 11_Cycle.js
+++ 11_Cycle.js
@@ -1,7 +1,24 @@
 // Циклы
 
+let a = 10;
+let b = 1;
+
 // While
+
+while (a<11) {
+    console.log(a);
+    a = a + 1;
+}
 
 // Do-while
 
+do {
+    console.log(b)
+    b = b + 1;
+} while (b <= 5);
+
 // For
+
+for (let i = 2; i <= 10; i = i + 1) {
+    console.log(i);   
+}
