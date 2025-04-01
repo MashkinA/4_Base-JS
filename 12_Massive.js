2b2207b5808dbd9761b891f5feb7165a115aea37
MashkinA <cool.solt12@ya.ru>
2025-04-01 21:22:08 +0300

    ed

--- 12_Massive.js
+++ 12_Massive.js
@@ -4,18 +4,49 @@
 
 // Объявление массива
 
-
+const number = 4;
+const names = [
+1,
+2,
+3,
+number
+];
+console.log(names);
 
 // Чтение данных из массива
 
-
+console.log(`Вывод из массива элемента с номером 2: ${names[2]}`);
+console.log(`Сколичество элементов массива: ${names.length}`);
 
 // Проход по элементам массива
 
-
+for (let index = 0; index < names.length; index++) {
+    const num = names[index];
+    console.log(num);
+}
 
 // for и foreach
 
+PrintNames (names);
 
+function PrintNames (name) {
+    name.forEach(function(item, index) { 
+        console.log(item, index)
+        }
+    );
+}
 
 // Поиск элемента в массиве
+
+find3 (names);
+
+function find3 (name) {
+    name.forEach(function(item, index) {
+        if (item === 3) {
+            
+        console.log(`${index} - Порядковый номер искомого числа`)
+            
+        } 
+        }
+    );
+}
