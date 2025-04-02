19d9638da8dc9439bb11f5c7d4e583d7a33ca053
MashkinA <cool.solt12@ya.ru>
2025-04-02 22:16:36 +0300

    ed

--- 14_Checkpoint.js
+++ 14_Checkpoint.js
@@ -1,6 +1,6 @@
 // 1
 
-// printNumbers ();
+printNumbers ();
 
 function printNumbers() {
     for (let a = -10; a <= 10; a++) {
@@ -13,7 +13,7 @@ function printNumbers() {
 const fnStart = -3
 const fnEnd = 5
 
-// printNumbersByRange(fnStart, fnEnd);
+printNumbersByRange(fnStart, fnEnd);
 
 function printNumbersByRange(start, end) {
     for (let a = start; a <= end; a++) {
@@ -25,7 +25,7 @@ function printNumbersByRange(start, end) {
 
 const num = [0,2,5,8,9];
 
-// calculateNumbersSum (num);
+calculateNumbersSum (num);
 
 function calculateNumbersSum(numbers) {
     let sum = 0;
@@ -52,11 +52,11 @@ function includes(numbers, number) {
 
 // 5
 
-const name = 'bob';
-const surname = 'smith';
-const age = 29;
+const name1 = 'bob';
+const surname1 = 'smith';
+const age1 = 29;
 
-console.log(createPerson(name, surname, age));
+console.log(createPerson(name1, surname1, age1));
 
 function createPerson(name, surname, age) {
     const person = {
@@ -65,7 +65,43 @@ function createPerson(name, surname, age) {
         age: age
     }
     return person;
-}
+};
 
+// 6
+
+const name2 = 'ban';
+const surname2 = 'walker';
+const age2 = 15;
+
+const name3 = 'jack';
+const surname3 = 'gill';
+const age3 = 46;
+
+people = [
+    createPerson(name1, surname1, age1),
+    createPerson(name2, surname2, age2),
+    createPerson(name3, surname3, age3),
+];
+
+console.log(findOldest (people));
+
+function findOldest(people) {
+    
+    let oldest = {
+        name: null,
+        age: null,
+    };
+       
+    for (let index = 0; index < people.length; index++) {
+        if (people[index].age > oldest.age) {
+            oldest = {
+                name: people[index].name,
+                age: people[index].age,
+            };
+        };
+    };
+    return oldest.name;
+    
+}
 
 
