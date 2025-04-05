45e6172e84e0356ab63242b203a6d1bbb6213e32
MashkinA <cool.solt12@ya.ru>
2025-04-05 15:38:54 +0300

    ed

--- Counter App/2_code.js
+++ Counter App/2_code.js
@@ -1,1 +1,15 @@
+let button = document.querySelector('.counter_button');
 let counter = 0;
+let counterText = document.querySelector('.counter_p')
+
+let button_reset = document.querySelector('.counter_reset');
+
+button.addEventListener('click', function () {
+    counter += 1;
+    counterText.innerText = counter;
+});
+
+button_reset.addEventListener('click', function () {
+    counter = 0;
+    counterText.innerText = counter;
+});
