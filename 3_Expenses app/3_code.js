a198f3ddb95af0da8bc5bf3b8512a69df0f30575
MashkinA <cool.solt12@ya.ru>
2025-04-08 16:12:48 +0300

    ed

--- 3_Expenses app/3_code.js
+++ 3_Expenses app/3_code.js
@@ -1,15 +1,45 @@
 const expenses = [];
+const limit = 10000;
 
 const inputNode = document.getElementById('input');
 const btnNode = document.getElementById('btn');
+const historyNode = document.getElementById('history')
+const sumNode = document.getElementById('sumer');
+const limitNode = document.getElementById('limit');
+const statusNode = document.getElementById('status');
+
+//
+limitNode.innerText = limit;
+//
 
 btnNode.addEventListener('click', function() {
     if (!inputNode.value) {
         return;
     }
-    const expence = parseInt(inputNode.value);
-    expenses.push(expence);
+    const expense = parseInt(inputNode.value);
     inputNode.value = '';
+    //
+    expenses.push(expense);
+    //
+    let expensesListHTML = '';
+    expenses.forEach(element => {
+        expensesListHTML += `<li>${element} руб.</li>`;
+    });
 
-    console.log(expenses);
+    historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+    //
+    let sum = 0;
+    expenses.forEach(element => {
+        sum += element;
+    });
+    sumNode.innerText = `${sum} руб.`;
+    
+    //
+    if (sum <= limit) {
+        statusNode.innerText = `Все хорошо`;
+    } else {
+        statusNode.innerText = `Лимит превышен`;
+    };
 });
+    
+    
