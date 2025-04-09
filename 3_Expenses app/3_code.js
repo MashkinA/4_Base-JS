678074aeb3e9f867e22b9a654809fa612bcba449
MashkinA <cool.solt12@ya.ru>
2025-04-09 13:57:59 +0300

    ed

--- 3_Expenses app/3_code.js
+++ 3_Expenses app/3_code.js
@@ -1,39 +1,71 @@
 let expenses = [];
-const limit = 10000;
 const currensy = 'руб.';
 const statusPositive = 'Все хорошо';
 const statusNegative = 'Лимит превышен';
+const storage_label_expenses = "expences";
 
 const inputNode = document.getElementById('input');
 const btnNode = document.getElementById('btn');
 const historyNode = document.getElementById('history')
 const sumNode = document.getElementById('sumer');
-const limitNode = document.getElementById('limit');
 const statusNode = document.getElementById('status');
 const resetBtnNode = document.getElementById('reset_btn');
+const categorySelectNode = document.getElementById('select');
+const limitBtnNode = document.getElementById('changeLimit');
+
+//
+const limitNode = document.getElementById('limit');
+limitNode.innerText = localStorage.getItem('limit');
+if (!localStorage.getItem('limit')) {
+    limitNode.innerText = 10000;
+};
+let limit = parseInt(limitNode.innerText);
+//
 
 initWallet();
 
 btnNode.addEventListener('click', function() {
-    expense = getExpenseByUser();
-    if (!expense) {
+    const fullExpense = getExpenseByUser();
+    if (!fullExpense) {
         return;
     };
     
-    trackExpense(expense);
+    trackExpense(fullExpense);
     
     render(expenses);
 });
+
+limitBtnNode.addEventListener('click', function() {
+    const new_limit = prompt('Введите новый лимит');
+    if (!new_limit) {
+        return;
+    };
+    const new_limit_value = parseInt(new_limit);
+
+    limitNode.innerText = new_limit_value;
+    limit = new_limit_value;
+    localStorage.setItem('limit', new_limit_value);
+
+    render(expenses);
+});
+
 resetBtnNode.addEventListener('click', function() {
     expenses = [];
+    expensesFromStorageObj = [];
     historyNode.innerHTML = '';
     initWallet();
 });
 
 function initWallet() {
     limitNode.innerText = `${limit} ${currensy}`;
-    sumNode.innerText = `-`;
-    statusNode.innerText = `Не определено`;
+    const expensesFromStorageStr = localStorage.getItem(storage_label_expenses);
+    const expensesFromStorageObj = JSON.parse(expensesFromStorageStr);
+    render(expensesFromStorageObj);
+    if (expensesFromStorageObj < 1) {
+        sumNode.innerText = `-`;
+        statusNode.innerText = `Не определено`;
+    };
+
 };
 
 function getExpenseByUser() {
@@ -42,14 +74,26 @@ function getExpenseByUser() {
     }
     const expense = parseInt(inputNode.value);
     clearInput();
-    return expense;
+
+    const currentCategory = getSelectedCategory();
+    if (currentCategory === 'Категория') {
+        return;
+    }
+    const fullExpense = {
+        amount: expense,
+        category: currentCategory,
+    };
+
+    return fullExpense;
 };
     function clearInput() {
         inputNode.value = '';
     };
 
-function trackExpense(expense) {
-    expenses.push(expense);
+function trackExpense(a) {
+    expenses.push(a);
+    const expStr = JSON.stringify(expenses);
+    localStorage.setItem(storage_label_expenses, expStr);
 };
 
 function render(expenses) {
@@ -62,7 +106,7 @@ function render(expenses) {
     function calculate(expenses) {
         let sum = 0;
         expenses.forEach(element => {
-            sum += element;
+            sum += element.amount;
         });
         return sum;
     };
@@ -70,7 +114,7 @@ function render(expenses) {
     function renderHistory (expenses) {
         let expensesListHTML = '';
         expenses.forEach(element => {
-            expensesListHTML += `<li>${element} ${currensy}</li>`;
+            expensesListHTML += `<li>${element.amount} ${currensy} - ${element.category}</li>`;
         });
 
         historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
@@ -89,6 +133,10 @@ function render(expenses) {
             statusNode.classList.add('status_red');
         };
     };
+    function getSelectedCategory() {
+        return categorySelectNode.value;
+    };
+
 
 
 
